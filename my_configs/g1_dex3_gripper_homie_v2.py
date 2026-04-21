341d4050e607d48db73526060ff33f5a39d522fc
Junwei Liang <junweiliang1114@gmail.com>
2026-04-21 14:47:57 +0800

    updated

---
+++ my_configs/g1_dex3_gripper_homie_v2.py
@@ -0,0 +1,57 @@
+from gr00t.configs.data.embodiment_configs import register_modality_config
+from gr00t.data.types import ModalityConfig, ActionConfig, ActionRepresentation, ActionType, ActionFormat
+from gr00t.data.embodiment_tags import EmbodimentTag
+
+g1_wbc_config = {
+    "video": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["cam_high"],
+    ),
+
+    # STATE: We feed the model EVERYTHING so it understands full body posture.
+    "state": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=[
+            "arms", "waist", "triggers", "loco_cmd", # 去掉hands, legs
+        ],
+        sin_cos_embedding_keys=["arms", "waist"],
+    ),
+
+    # ACTION: We restrict the model to ONLY output what you requested.
+    "action": ModalityConfig(
+        delta_indices=list(range(0, 50)),
+        modality_keys=[
+            "arms",
+            "waist",
+            "triggers",
+            "loco_cmd"
+        ],
+        action_configs=[
+            # Arms (14D): Smooth relative joint control
+            ActionConfig(
+                rep=ActionRepresentation.RELATIVE,
+                type=ActionType.NON_EEF, format=ActionFormat.DEFAULT),
+            # waist (3D): Smooth relative joint control
+            ActionConfig(
+                rep=ActionRepresentation.RELATIVE,
+                type=ActionType.NON_EEF, format=ActionFormat.DEFAULT),
+
+            # Triggers (2D): Absolute target state (0.0 to 1.0)
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF, format=ActionFormat.DEFAULT),
+
+            # Loco Commands (4D): Absolute target velocities
+            ActionConfig(
+                rep=ActionRepresentation.ABSOLUTE,
+                type=ActionType.NON_EEF, format=ActionFormat.DEFAULT),
+        ],
+    ),
+
+    "language": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["annotation.human.task_description"],
+    ),
+}
+
+register_modality_config(g1_wbc_config, embodiment_tag=EmbodimentTag.NEW_EMBODIMENT)
--- note_junwei.md
+++ note_junwei.md
@@ -2,8 +2,14 @@
     + fork 在precognitionlab下：`https://github.com/precognitionlab/Isaac-GR00T-N1.7`
 ```
 
-    # 安装
+    # 安装 (gpu3, office)
+        $ sudo apt install git-lfs && git lfs install
+        $ sudo apt-get update && sudo apt-get install -y ffmpeg
         $ git submodule update --init --recursive
+
+            # gpu3 安装需要把 aarch64 的一些路径删除
+            # gpu3速度慢，要清华园UV_DEFAULT_INDEX="https://pypi.tuna.tsinghua.edu.cn/simple"
+
         junweil@office-precognition:~/projects/wbc_manipulation/Isaac-GR00T-N1.7$ uv sync --python 3.10
 
         # 还需要账号同意cosmos-reason条款:
@@ -13,6 +19,8 @@
             uv pip install -U "huggingface_hub[cli]"
 
             uv run huggingface-cli login
+
+                # gpu3网络有问题，还要加这个 export HF_ENDPOINT=https://hf-mirror.com
 
             ~/Desktop/github_projects/huggingface_key.txt
 
@@ -33,5 +41,13 @@
             # data num worker最好设置6， 比4要快
 
         junweil@office-precognition:~/projects/wbc_manipulation/Isaac-GR00T-N1.7$ PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True uv run torchrun --nproc_per_node=2 gr00t/experiment/launch_finetune.py      --base-model-path ../GR00T-N1.7-3B/      --dataset-path ~/.cache/huggingface/lerobot/junweiliang/wbc_pick_up_object_from_ground      --embodiment-tag NEW_EMBODIMENT      --modality-config-path my_configs/g1_dex3_gripper_homie.py      --save-total-limit 2      --learning_rate 1e-4      --save-steps 2000      --max-steps 20000      --use-wandb      --warmup_ratio 0.05      --weight_decay 1e-5      --global-batch-size 128    --color-jitter-params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08      --dataloader-num-workers 6      --output-dir experiments/my_wbc_pick_up_object_from_ground_bs128_s20k
+
+            # 1xA6000 48GB, bs=128 也可以跑，41 GB/48GB, 11小时, 搞笑，不要用多卡了
+            # 1xA6000 48GB, bs=256 OOM
+            # 2xA6000 48GB, bs=256 OOM, 代码可能有问题
+
+    # [04/21/2026] 实验，使用新的g1_dex3_gripper_homie_v2.py，states与action输出保持一致, action chunk length=50。
+
+        # bs=128, lr=5e-5, 20k step
 
 ```
